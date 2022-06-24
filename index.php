d7496d585732e801646e66798a9ffad00844312d
fertiort <fertiort>
2022-06-24 10:00:51 +0300

    rectif
    navigation et slider

--- index.php
+++ index.php
@@ -4,77 +4,10 @@
 get_header();
 ?>
 
-<<<<<<< HEAD
-<!--
-<<<<<<< HEAD
--->
-<div class="menu">
-    <nav id="liste" class="nav">
-            <ul>
-                <li>
-                    <a href="index.php">Acceuil</a>
-                </li>
-                <li>
-                    <a href="universite.php">L'université</a>
-                </li>
-                <li>
-                    <a href="vie_etudiante.php">Vie étudiante</a>
-                </li>
-                <li>
-                    <a href="ressources.php">Ressources</a>
-                </li>
-                <li>
-                    <a href="projet.php">Projets et innovations climats</a>
-                </li>
-                <li>
-                    <a href="contacts.php">Contacts</a>
-                </li>
-            </ul>
-        </nav>
-    </div>
-<!--======= 
+
+
 <?php include("slider.php"); ?>
 
-
->>>>>>> 9b7f3615c53765de129db0b99448758745849afc
--->
-    <div class="diaporama">
-        <ul id="diapo">
-        <img src="images/prev.png" alt="" id="b_prev">
-            <li>
-                <img src="images/photo2.jpg" class="image">
-            </li>
-            <li>
-                <img src="images/photo3.jpg" class="image">
-            </li>
-            <li>
-                <img src="images/photo4.jpg" class="image">
-            </li>
-            <li>
-                <img src="images/photo5.jpg" class="image">
-            </li>
-        <img src="images/next.png" alt="" id="b_next">
-        </ul>
-        <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus animi adipisci ipsa laudantium officia, odit atque laboriosam culpa sapiente. Eos veritatis nobis perferendis placeat commodi saepe harum cupiditate laboriosam? Doloribus.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea sapiente veritatis aut, temporibus asperiores harum totam sed tenetur vero. Alias nihil amet omnis fugiat deleniti cumque ab magni numquam. <br>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus animi adipisci ipsa laudantium officia, odit atque laboriosam culpa sapiente. Eos veritatis nobis perferendis placeat commodi saepe harum cupiditate laboriosam? Doloribus.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea sapiente veritatis aut, temporibus asperiores harum totam sed tenetur vero. Alias nihil amet omnis fugiat deleniti cumque ab magni numquam.
-        </p>
-        
-    </div>
-
-<!--
-<<<<<<< HEAD
-=======
-
-	
->>>>>>> 9b7f3615c53765de129db0b99448758745849afc
--->
-<!--  =======
->>>>>>> parent of 126125a (Styleintegration)
-	
--->
 
 <?php
 get_footer();
--- slider.php
+++ slider.php
@@ -1,4 +1,27 @@
-<div>
+
     <!-- image en defilement pour presenter le site -->
-    slide
+<div class="diaporama">
+    <ul id="diapo">
+    <img src="images/prev.png" alt="" id="b_prev">
+        <li>
+            <img src="images/photo2.jpg" class="image">
+        </li>
+        <li>
+            <img src="images/photo3.jpg" class="image">
+        </li>
+        <li>
+            <img src="images/photo4.jpg" class="image">
+        </li>
+        <li>
+            <img src="images/photo5.jpg" class="image">
+        </li>
+    <img src="images/next.png" alt="" id="b_next">
+    </ul>
+    <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus animi adipisci ipsa laudantium officia, odit atque laboriosam culpa sapiente. Eos veritatis nobis perferendis placeat commodi saepe harum cupiditate laboriosam? Doloribus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea sapiente veritatis aut, temporibus asperiores harum totam sed tenetur vero. Alias nihil amet omnis fugiat deleniti cumque ab magni numquam. <br>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus animi adipisci ipsa laudantium officia, odit atque laboriosam culpa sapiente. Eos veritatis nobis perferendis placeat commodi saepe harum cupiditate laboriosam? Doloribus.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ea sapiente veritatis aut, temporibus asperiores harum totam sed tenetur vero. Alias nihil amet omnis fugiat deleniti cumque ab magni numquam.
+    </p>
+    
 </div>
